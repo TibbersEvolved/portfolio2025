17a2199474a113324b652c91fc625f080415db7a
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-04-24 21:43:08 +0200

    Configured CI/CD

--- portfolio-app/src/routes/__root.tsx
+++ portfolio-app/src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/shared/footer";
 import Navbar from "@/components/shared/navbar";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
