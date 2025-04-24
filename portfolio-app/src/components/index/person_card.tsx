389c7d00d0a52ee6e2e1b4cdb4154fb59b2e1a6c
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-04-24 15:14:46 +0200

    Added profile picture and styled the image

--- portfolio-app/src/components/index/person_card.tsx
+++ portfolio-app/src/components/index/person_card.tsx
@@ -1,10 +1,10 @@
-import myImage from "../../assets/linkedinImg.png";
+import myImage from "../../assets/erik2.png";
 
 export default function Person_Card() {
   return (
     <>
       <section className="bg-zinc-50 shadow-md flex gap-5 p-4">
-        <img src={myImage} className="size-40"></img>
+        <img src={myImage} className="size-40 rounded-2xl shadow-md"></img>
         <aside className="flex flex-col">
           <div>Erik Olin</div>
           <div>Fullstack Java Developer</div>
