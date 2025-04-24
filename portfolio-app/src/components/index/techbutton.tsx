91550434285c9f593803c66129f747e8999f1796
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-04-24 19:14:30 +0200

    Styling updates to Techbutton and Project Card

--- portfolio-app/src/components/index/techbutton.tsx
+++ portfolio-app/src/components/index/techbutton.tsx
@@ -1,7 +1,7 @@
 export default function Techbutton(prop: props) {
   return (
     <>
-      <div className="flex gap-0.5 p-1 bg-zinc-300 border-2 border-zinc-400 rounded-md hover:bg-zinc-200">
+      <div className="flex gap-0.5 p-1 bg-gradient-to-b from-zinc-100 to-emerald-200 border-2 border-emerald-300 rounded-sm hover:to-emerald-100">
         <img src={prop.image} className="size-6  rounded-md"></img>
         <div className="text-sm">{prop.name}</div>
       </div>
--- portfolio-app/src/components/projects/projectCard.tsx
+++ portfolio-app/src/components/projects/projectCard.tsx
@@ -1,9 +1,9 @@
 export default function ProjectCard(props: projectInfo) {
   return (
     <>
-      <section className="flex flex-col size-60">
-        <img className="h-1/2" src={props.image}></img>
-        <div>
+      <section className="flex flex-col size-60 bg-zinc-50 shadow-md rounded-2xl hover:bg-zinc-100">
+        <img className="h-1/2 rounded-t-2xl" src={props.image}></img>
+        <div className="p-2">
           <header className="text-lg font-semibold">{props.title}</header>
           <div>{props.desc}</div>
         </div>
