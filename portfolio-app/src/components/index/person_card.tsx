0ea96ff6fb274788d7a4a49f923352155d0876a3
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-04-24 23:14:59 +0200

    Temp fix to email adress breaking mobile view

--- portfolio-app/src/components/index/person_card.tsx
+++ portfolio-app/src/components/index/person_card.tsx
@@ -4,7 +4,10 @@ export default function Person_Card() {
   return (
     <>
       <section className="bg-zinc-50 shadow-md rounded-md flex text-sm md:text-base gap-5 p-4">
-        <img src={myImage} className="size-40 rounded-2xl shadow-md"></img>
+        <img
+          src={myImage}
+          className="size-30 md:size-40 rounded-2xl shadow-md"
+        ></img>
         <aside className="flex flex-col">
           <div>Erik Olin</div>
           <div>Fullstack Java Developer</div>
@@ -13,7 +16,7 @@ export default function Person_Card() {
         <section className="flex flex-col text-xs md:text-sm ml-auto mr-4">
           <div>Stockholm - Sweden</div>
           <div>+4676-5528712</div>
-          <div className="size-min">erik.olin.spanga@gmail.com</div>
+          <div className="hidden md:inline">erik.olin.spanga@gmail.com</div>
         </section>
       </section>
     </>
