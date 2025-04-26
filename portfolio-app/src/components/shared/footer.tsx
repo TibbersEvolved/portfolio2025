54927296ad376997979efb208ea7a59718915e69
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-04-26 14:48:33 +0200

    Changed footer to gradient variant

--- portfolio-app/src/components/shared/footer.tsx
+++ portfolio-app/src/components/shared/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <section className="flex static w-auto h-20 bg-slate-400 justify-center"></section>
+      <section className="flex static w-auto h-30 bg-gradient-to-b from-zinc-100 to-zinc-400 justify-center"></section>
     </>
   );
 }
