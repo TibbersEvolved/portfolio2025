9d73939cb01fb41e10e93872081dffcf45452a64
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-04-29 13:45:19 +0200

    Added a filter method to skill db and a select to homepage

--- portfolio-app/src/components/index/techstack.tsx
+++ portfolio-app/src/components/index/techstack.tsx
@@ -1,15 +1,29 @@
 import Techbutton from "./techbutton";
-import { getAllSkills, type skill } from "../data/skillDB";
+import { getAllSkills, getSkillsByCategory, type skill } from "../data/skillDB";
+import { useState } from "react";
 
 export default function Techstack() {
+  const [filter] = useState("core");
   const listTech: skill[] = getAllSkills();
-
+  const listCore = getSkillsByCategory(listTech, filter);
   return (
     <>
       <section className="bg-zinc-50 shadow-md gap-5 p-4 flex flex-col mt-4">
-        <div className="text-xl font-bold">Main Technologies</div>
+        <div className="text-xl font-bold flex gap-6">
+          <div>Technologies:</div>{" "}
+          <select
+            name="selectFilter"
+            defaultValue={"core"}
+            className="bg-zinc-100 shadow-sm border-2 border-zinc-200 hover:border-amber-200"
+          >
+            Core
+            <option value={"core"}>Core</option>
+            <option value={"frontend"}>Frontend</option>
+            <option value={"backend"}>Backend</option>
+          </select>
+        </div>
         <div className="flex flex-row gap-2 flex-wrap">
-          {listTech.map((skill, key) => {
+          {listCore.map((skill, key) => {
             return <Techbutton skill={skill} key={key} />;
           })}
         </div>
