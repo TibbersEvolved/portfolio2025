69a5ad4c0398868dcb7508144a5948df413f12ef
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-04-29 14:02:17 +0200

    Fully implemented filter

--- portfolio-app/src/components/index/techstack.tsx
+++ portfolio-app/src/components/index/techstack.tsx
@@ -3,9 +3,15 @@ import { getAllSkills, getSkillsByCategory, type skill } from "../data/skillDB";
 import { useState } from "react";
 
 export default function Techstack() {
-  const [filter] = useState("core");
+  const [filter, selectFilter] = useState("core");
   const listTech: skill[] = getAllSkills();
   const listCore = getSkillsByCategory(listTech, filter);
+
+  function onSelect(event: React.ChangeEvent<HTMLSelectElement>) {
+    event.preventDefault();
+    selectFilter(event.target.value);
+  }
+
   return (
     <>
       <section className="bg-zinc-50 shadow-md gap-5 p-4 flex flex-col mt-4">
@@ -15,6 +21,7 @@ export default function Techstack() {
             name="selectFilter"
             defaultValue={"core"}
             className="bg-zinc-100 shadow-sm border-2 border-zinc-200 hover:border-amber-200"
+            onChange={(e) => onSelect(e)}
           >
             Core
             <option value={"core"}>Core</option>
